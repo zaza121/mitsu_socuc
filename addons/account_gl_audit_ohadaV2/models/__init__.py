e295ba2753273a4e197a7b42ec2bb0d862bae788
mitsukitechnologie <mitsukitechnologie@gmail.com>
2025-10-22 20:00:56 +0200

    LM

--- addons/account_gl_audit_ohadaV2/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -- coding: utf-8 --
-
-from . import audit_grand_livre
--- addons/account_gl_audit_v4/__manifest__.py
+++ addons/account_gl_audit_v4/__manifest__.py
@@ -1,12 +1,13 @@
-# -- coding: utf-8 --
+# -*- coding: utf-8 -*-
 
 {
     "name": "Audit Grand Livre SYSCOHADA (Optimisé)",
-    "version": "1.0.2",
+    "version": "1.0.3",
     "category": "Accounting",
     "summary": "Audit Grand Livre SYSCOHADA avec périodes, classes de comptes et export Excel (Odoo 18 optimisé)",
     "depends": ["account"],
     "data": [
+        "security/ir.model.access.csv",  <-- NOUVELLE LIGNE AJOUTÉE
         "views/audit_grand_livre_views.xml",
     ],
     "installable": True,
