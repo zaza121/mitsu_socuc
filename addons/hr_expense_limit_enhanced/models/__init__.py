790ad6645f731223ba1e6253e7e6191a5dc8920b
mitsukitechnologie <mitsukitechnologie@gmail.com>
2025-10-22 20:28:44 +0200

    E

--- addons/hr_expense_limit_enhanced/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import hr_employee
-from . import hr_expense_sheet
-from . import hr_expense
-from . import hr_job
-from . import product_template
--- addons/hr_expense_limit_enhanced/models/product_template.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    expense_limit = fields.Float(
-        string="Limite de dépense",
-        help="Montant maximum autorisé pour cette catégorie de dépense."
-    )
