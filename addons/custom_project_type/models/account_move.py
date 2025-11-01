0925ef327b7b453c0dbd85f87dbf9fe74add2cfe
mitsukitechnologie <mitsukitechnologie@gmail.com>
2025-11-01 15:10:57 +0100

    ZZ

--- addons/custom_project_type/models/account_move.py
+++ addons/custom_project_type/models/account_move.py
@@ -2,27 +2,27 @@ from odoo import models, _
 from odoo.exceptions import UserError
 
 class AccountMove(models.Model):
-_inherit = 'account.move'
-
-def _get_project_from_distribution(self, distribution):
-if not distribution:
-return False
-try:
-keys = list(distribution.keys())
-except Exception:
-return False
-if not keys:
-return False
-project = self.env['project.project'].search([
-('analytic_account_id', 'in', keys)
-], limit=1)
-return project
-
-def _check_internal_project_revenue(self):
-for move in self:
-if move.move_type in ('out_invoice', 'out_refund'):
-for line in move.invoice_line_ids:
-dist = line.analytic_distribution if hasattr(line, 'analytic_distribution') else False
-project = self._get_project_from_distribution(dist)
-if project and project.project_type == 'internal':
-raise UserError(_("Les projets internes ne peuvent pas générer de revenus (Factures Clients)."))
+    _inherit = 'account.move'
+    
+    def _get_project_from_distribution(self, distribution):
+        if not distribution:
+            return False
+        try:
+            keys = list(distribution.keys())
+        except Exception:
+            return False
+        if not keys:
+            return False
+        project = self.env['project.project'].search([
+            ('analytic_account_id', 'in', keys)
+        ], limit=1)
+        return project
+    
+    def _check_internal_project_revenue(self):
+        for move in self:
+            if move.move_type in ('out_invoice', 'out_refund'):
+                for line in move.invoice_line_ids:
+                    dist = line.analytic_distribution if hasattr(line, 'analytic_distribution') else False
+                    project = self._get_project_from_distribution(dist)
+                    if project and project.project_type == 'internal':
+                        raise UserError(_("Les projets internes ne peuvent pas générer de revenus (Factures Clients)."))
--- addons/custom_project_type/models/project_project.py
+++ addons/custom_project_type/models/project_project.py
@@ -1,16 +1,16 @@
 from odoo import models, fields, api
 
 class ProjectProject(models.Model):
-_inherit = 'project.project'
-
-project_type = fields.Selection([
-('external', 'Externe (Facturable)'),
-('internal', 'Interne (Dépenses uniquement)')
-], string="Type de Projet", default='external', required=True)
-
-@api.model
-def create(self, vals):
-if vals.get('sale_line_id') or self.env.context.get('default_sale_order_id'):
-vals = dict(vals)
-vals['project_type'] = 'external'
-return super(ProjectProject, self).create(vals)
+    _inherit = 'project.project'
+    
+    project_type = fields.Selection([
+        ('external', 'Externe (Facturable)'),
+        ('internal', 'Interne (Dépenses uniquement)')
+    ], string="Type de Projet", default='external', required=True)
+    
+    @api.model
+    def create(self, vals):
+        if vals.get('sale_line_id') or self.env.context.get('default_sale_order_id'):
+            vals = dict(vals)
+            vals['project_type'] = 'external'
+        return super(ProjectProject, self).create(vals)
